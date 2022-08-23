7d1f2482a0d153f2b576ccdcc90ac9521285300f
olivinny <vinicius.gtoliv@gmail.com>
2022-08-23 19:20:04 -0300

    Exercicios Components

--- src/App.js
+++ src/App.js
@@ -1,50 +1,14 @@
-import "./App.css";
-
-// Organize os produtos como mostrado no vídeo
-// Mostre apenas produtos que forem mais caros que R$ 1500
-
-const produtos = [
-  {
-    id: 1,
-    nome: "Smartphone",
-    preco: "R$ 2000",
-    cores: ["#29d8d5", "#252a34", "#fc3766"],
-  },
-  {
-    id: 2,
-    nome: "Notebook",
-    preco: "R$ 3000",
-    cores: ["#ffd045", "#d4394b", "#f37c59"],
-  },
-  {
-    id: 3,
-    nome: "Tablet",
-    preco: "R$ 1500",
-    cores: ["#365069", "#47c1c8", "#f95786"],
-  },
-];
+import Footer from "./Exercicios/Components/Ex-Components1/Footer";
+import Form from "./Exercicios/Components/Ex-Components1/Form/Form";
+import Header from "./Exercicios/Components/Ex-Components1/Header";
 
 const App = () => {
-  const dados = produtos.filter(
-    ({ preco }) => Number(preco.replace("R$ ", "")) > 1500
-  );
-
   return (
-    <section>
-      {dados.map(({ id, nome, preco, cores }) => (
-        <div key={id}>
-          <h1>{nome}</h1>
-          <p>Preço: {preco}</p>
-          <ul>
-            {cores.map((cor) => (
-              <li style={{ backgroundColor: cor, color: "white" }} key={cor}>
-                {cor}
-              </li>
-            ))}
-          </ul>
-        </div>
-      ))}
-    </section>
+    <>
+      <Header />
+      <Form />
+      <Footer />
+    </>
   );
 };
 export default App;
---
+++ src/Exercicios/Components/Components.js
@@ -0,0 +1,14 @@
+import Footer from "./Ex-Components1/Footer";
+import Form from "./Ex-Components1/Form/Form";
+import Header from "./Ex-Components1/Header";
+
+const App = () => {
+  return (
+    <>
+      <Header />
+      <Form />
+      <Footer />
+    </>
+  );
+};
+export default App;
